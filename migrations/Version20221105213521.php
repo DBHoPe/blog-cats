bc34c2a234c4875a574617e1e47f57723108f65e
Jan Petrak <jan.petrak@lyfle.com>
2022-11-05 22:48:57 +0100

    create initial migration

---
+++ migrations/Version20221105213521.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20221105213521 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE cat (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, birth_date DATE NOT NULL, description VARCHAR(255) NOT NULL, image_path VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE cat_owner (cat_id INT NOT NULL, owner_id INT NOT NULL, INDEX IDX_731B5691E6ADA943 (cat_id), INDEX IDX_731B56917E3C61F9 (owner_id), PRIMARY KEY(cat_id, owner_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE owner (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE cat_owner ADD CONSTRAINT FK_731B5691E6ADA943 FOREIGN KEY (cat_id) REFERENCES cat (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE cat_owner ADD CONSTRAINT FK_731B56917E3C61F9 FOREIGN KEY (owner_id) REFERENCES owner (id) ON DELETE CASCADE');
+        $this->addSql('DROP TABLE test');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE test (a VARCHAR(10) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_general_ci`, b INT DEFAULT NULL) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_general_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE cat_owner DROP FOREIGN KEY FK_731B5691E6ADA943');
+        $this->addSql('ALTER TABLE cat_owner DROP FOREIGN KEY FK_731B56917E3C61F9');
+        $this->addSql('DROP TABLE cat');
+        $this->addSql('DROP TABLE cat_owner');
+        $this->addSql('DROP TABLE owner');
+    }
+}
